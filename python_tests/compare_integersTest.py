8170dac910a87fea1e305246c647e7f9025073f0
Danielle <daniellebuggle7@gmail.com>
2025-01-15 22:50:00 +0000

    added test file for compare_integers student exercise

---
+++ python_tests/compare_integersTest.py
@@ -0,0 +1,13 @@
+import unittest
+class compare_integersTest(unittest.TestCase):
+    def test_equal(self):
+        """Check when both integers are equal."""
+        self.assertEqual(compare_integers(10, 10), 1)  # a == b
+
+    def test_a_greater(self):
+        """Check when the first integer is greater."""
+        self.assertEqual(compare_integers(20, 10), 2)  # a > b
+
+    def test_b_greater(self):
+        """Check when the second integer is greater."""
+        self.assertEqual(compare_integers(5, 15), 3)  # b > a
